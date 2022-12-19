f0fa38b687cf5f48782ad829c7b22720437bca70
khaledabdrabo98 <khaledm@rocketmail.com>
2022-12-19 03:45:35 +0100

    Use PlantId API to identify plant from image + refactor back

--- api/app.js
+++ api/app.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const path = require('path');
-// const plantRoute = require('./routes/plant');
+var multer = require('multer');
+const https = require('https');
+var fs = require('fs');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded());
+
+//Setting storage engine
+const storageEngine = multer.diskStorage({
+  destination: "./api/images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+
+const checkFileType = function (file, cb) {
+  //Allowed file extensions
+  const fileTypes = /jpeg|jpg|png/;
+
+  //check extension names
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {  
+    cb("Error: You can Only Upload Images!!");
+  }
+};
+
+const upload = multer({
+  storage: storageEngine,
+  limits: { fileSize: 10000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -12,58 +47,63 @@ app.use(function(req, res, next) {
   next();
 });
 
-// app.use('/images', express.static(path.join(__dirname, 'images')));
-// app.use('/plant', plantRoute);
+// Route to get plant info (from image)
+// API used to get plant name and info : https://plant.id/
+app.post("/plant/photo", upload.single("image"), (req, res) => {
+  if (req.file) {
+    const files = [ req.file.path ]; 
+    const base64files = files.map(file => fs.readFileSync(file, 'base64'));
 
-const https = require('https');
-var fs = require('fs');
-
-const files = ['api/images/exemple1.jpg'];
-
-const base64files = files.map(file => fs.readFileSync(file, 'base64'));
-
-const plant_id_data = JSON.stringify({
-    api_key: "LaMbBrSsOxSRAcbuWPy8GiPWwGvf1YdOefzKQzgYHnATrbhmhr",
-    images: base64files,
-    /* modifiers info: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers */
-    modifiers: ["crops_fast", "similar_images"],
-    plant_language: "en",
-    /* plant details info: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details */
-    plant_details: ["common_names",
-        "url",
-        "name_authority",
-        "wiki_description",
-        "taxonomy",
-        "synonyms"],
-});
-
-const options = {
-    hostname: 'api.plant.id',
-    port: 443,
-    path: '/v2/identify',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': plant_id_data.length
-    }
-}
-
-const req = https.request(options, res => {
-    res.on('data', d => {
-        process.stdout.write(d)
-        //req.end()
+    const plant_id_data = JSON.stringify({
+        api_key: "qhxxYbzOI1LgAt0y7wacRHc7nfZhr67A7ScBEQaKkUK2ZZLwwO",
+        images: base64files,
+        /* modifiers info: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers */
+        modifiers: ["crops_fast", "similar_images"],
+        plant_language: "en",
+        /* plant details info: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details */
+        plant_details: ["common_names",
+            "url",
+            "name_authority",
+            "wiki_description",
+            "taxonomy",
+            "synonyms"],
     });
+
+    const options = {
+        hostname: 'api.plant.id',
+        port: 443,
+        path: '/v2/identify',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': plant_id_data.length
+        }
+    }
+
+    var message = "";
+    req = https.request(options, response => {
+      response.on('data', d => {
+          // process.stdout.write(d);
+          message += d; 
+      }).on('end', () => {
+          res.json({ message: JSON.parse(message).suggestions });  
+      });
+    });
+    
+    req.on('error', error => {
+      console.error('Error: ', error)
+    });
+
+    req.write(plant_id_data)
+    req.end()
+
+  } else {
+    res.status(400).send("Please upload a valid image");
+  }
 });
 
-req.on('error', error => {
-  console.error('Error: ', error)
-  // res.end()
-});
-
-// Route to get plant name (from image)
-app.post("/plant/photo", (req, res, next) => {
-  res.write(plant_id_data)
-  // 
+app.post("/plant/id", (req, res, next) => {
+  // Next TODO
 });
 
 app.use((req, res) => {
--- api/routes/plant.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const plantController = require('../controllers/plant');
-
-const router = express.Router();
-
-router.post('/photo', plantController.searchPlant);
-
-module.exports = router;
